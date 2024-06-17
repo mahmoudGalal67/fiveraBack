705fca7eb92c4ec1d11f39f9d7a017e71d4b172b
mahmoudGalal67 <112634908+mahmoudGalal67@users.noreply.github.com>
2024-06-17 16:48:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -24,9 +24,7 @@ const connect = async () => {
     console.log(error);
   }
 };
-
-// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use(cors());
+ app.use(cors({credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
